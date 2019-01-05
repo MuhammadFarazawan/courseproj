cf608ccfdaf460925f0a901372d5ee51c80ff5ad
MuhammadFarazawan <42037361+MuhammadFarazawan@users.noreply.github.com>
2019-01-05 11:57:54 +0500

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,37 @@
+featuresTest<-read.table("assgn/test/x_test.txt", header = FALSE)
+featuresTrain<-read.table("assgn/train/x_train.txt", header = FALSE)
+subject<-rbind(subjectTrain, subjecttest)
+activity<-rbind(activityTrain, activityTets)
+features<-rbind(featuresTrain, featuresTest)
+colnames(features)<-t(featureNames[2])
+colnames(activity)<-"Activity"
+colnames(subject)<-"Subject"
+completeData<-cbind(features, activity, subject)
+columnsWithMeanSTD <- grep(".*Mean.*|.*Std.*", names(completeData), ignore.case=TRUE)
+requiredColumns <- c(columnsWithMeanSTD, 562, 563)
+dim(completeData)
+extractedData <- completeData[,requiredColumns]
+dim(extractedData)
+extractedData$Activity <- as.character(extractedData$Activity)
+for (i in 1:6){
+  extractedData$Activity[extractedData$Activity == i] <- as.character(activityLabels[i,2])
+}
+extractedData$Activity <- as.factor(extractedData$Activity)
+names(extractedData)
+names(extractedData)<-gsub("Acc", "Accelerometer", names(extractedData))
+names(extractedData)<-gsub("Gyro", "Gyroscope", names(extractedData))
+names(extractedData)<-gsub("BodyBody", "Body", names(extractedData))
+names(extractedData)<-gsub("Mag", "Magnitude", names(extractedData))
+names(extractedData)<-gsub("^t", "Time", names(extractedData))
+names(extractedData)<-gsub("^f", "Frequency", names(extractedData))
+names(extractedData)<-gsub("tBody", "TimeBody", names(extractedData))
+names(extractedData)<-gsub("-mean()", "Mean", names(extractedData), ignore.case = TRUE)
+names(extractedData)<-gsub("-std()", "STD", names(extractedData), ignore.case = TRUE)
+names(extractedData)<-gsub("-freq()", "Frequency", names(extractedData), ignore.case = TRUE)
+names(extractedData)<-gsub("angle", "Angle", names(extractedData))
+names(extractedData)<-gsub("gravity", "Gravity", names(extractedData))
+extractedData$Subject <- as.factor(extractedData$Subject)
+extractedData <- data.table(extractedData)
+tidyData <- aggregate(. ~Subject + Activity, extractedData, mean)
+tidyData <- tidyData[order(tidyData$Subject,tidyData$Activity),]
+write.table(tidyData, file = "Tidy.txt", row.names = FALSE)
